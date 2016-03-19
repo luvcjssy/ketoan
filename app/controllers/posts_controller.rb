4bd420cf51604b37d9a5f8709301707eaefbec34
Hoan Nguyen <nguyenhoan2510@gmail.com>
2016-03-19 20:16:50 +0700

    list post on index and create intro page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,10 @@
 class PostsController < ApplicationController
   def index
+  	@recruitments = Post.where(:category_id => 33).order(:created_at => 'DESC').limit(3)
+  	@recent_news = Post.all.order(:created_at => 'DESC').limit(6)
   end
+
+  def intro
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   resources :posts, only: [:index]
+  get 'intro/index'
+  resources :categories, only: [:show]
 
   root 'posts#index'
 
