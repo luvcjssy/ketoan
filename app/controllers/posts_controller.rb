93c37d6dbfe0b485ba4847eec9023a683af9800e
Hoan Nguyen <nguyenhoan2510@gmail.com>
2016-03-20 13:42:05 +0700

    id category for local version

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-  	@posts = Post.where(:active => true, :category_id => 12).order(:created_at => 'DESC').limit(3)
+  	@posts = Post.where(:active => true, :category_id => 33).order(:created_at => 'DESC').limit(3)
   	@recent_news = Post.where(:active => true).order(:created_at => 'DESC').limit(6)
   end
 
